7dfc777d7d49a150cf6efbd9532614d4ad20ed59
Evelyn Utterson <e.utterson@hotmail.co.uk>
2016-02-29 16:08:28 +0000

    added books create - tested in insomnia

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,8 +1,20 @@
 class BooksController < ApplicationController
 
+  skip_before_filter :verify_authenticity_token
+
   def index
     books = Book.all
     render :json => books.to_json()
+  end
+
+  def create
+    Book.create(book_params)
+    books = Book.all
+    render :json => books.to_json()
+  end
+
+  def book_params
+    params.require(:book).permit(:title, :author, :cover_image, :genre, :language, :difficulty_level, :difficulty_desc, :description)
   end
 
 end
