fa789cf8c8e87b6dcccd9a67e549b313d5a8104c
Evelyn Utterson <e.utterson@hotmail.co.uk>
2016-02-29 16:15:29 +0000

    added books show - tested in insomnia

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -13,6 +13,11 @@ class BooksController < ApplicationController
     render :json => books.to_json()
   end
 
+  def show
+    book = Book.find(params[:id])
+    render :json => book.to_json()
+  end
+
   def book_params
     params.require(:book).permit(:title, :author, :cover_image, :genre, :language, :difficulty_level, :difficulty_desc, :description)
   end
