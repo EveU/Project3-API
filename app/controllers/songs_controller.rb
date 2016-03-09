55f46a0a54c5d16553024b3e8029a746114e8171
Evelyn Utterson <e.utterson@hotmail.co.uk>
2016-03-09 18:24:16 +0000

    added songs model and controlled. index route working, tested in insomnia

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,15 @@
+class SongsController < ApplicationController
+
+  skip_before_filter :verify_authenticity_token
+
+  def index
+    songs = Song.all
+    render :json => songs.to_json()
+  end
+
+
+  def song_params
+    params.require(:song).permit(:title, :artist, :genre, :language, :difficulty, :description, :video_url)
+  end
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160229142617) do
+ActiveRecord::Schema.define(version: 20160309180143) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,18 @@ ActiveRecord::Schema.define(version: 20160229142617) do
     t.text     "description"
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
+  end
+
+  create_table "songs", force: :cascade do |t|
+    t.text     "title"
+    t.text     "artist"
+    t.text     "genre"
+    t.text     "language"
+    t.text     "difficulty"
+    t.text     "description"
+    t.text     "video_url"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
 end
